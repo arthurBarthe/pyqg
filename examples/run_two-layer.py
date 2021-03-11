c5561c288422b1297db16af7e44cf5788230b791
Arthur <arthur.guillaumin@gmail.com>
2021-03-11 16:22:11 -0500

    Change high-rez and low-rez grids

--- examples/run_two-layer.py
+++ examples/run_two-layer.py
@@ -2,13 +2,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pyqg
 
-m = pyqg.QGModel(tavestart=0,  dt=8000, nx=64 // 4, ny=64 // 4, L=64*1e4)
+m = pyqg.QGModel(tavestart=0,  dt=8000, nx=256 // 1, L = 1e6,
+                 filterfac=23.6)
+
 for snapshot in m.run_with_snapshots(
         tsnapstart=0, tsnapint=100*m.dt):
     plt.clf()
     plt.imshow(m.q[0] + m.Qy1 * m.y)
-    plt.clim([0,  m.Qy1 * m.W])
+    plt.clim([0, m.Qy1 * m.W])
+    plt.colorbar()
     plt.pause(0.01)
     plt.draw()
-    
 # now the model is done
